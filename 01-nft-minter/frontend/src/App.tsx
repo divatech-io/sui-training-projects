a26034ecb4d1ba0744d99b8cf999526255239aa0
daniilkananenka <neokv210@gmail.com>
2025-09-27 23:01:35 +0300

    feat: (01-nt-minter) ready - no blockchain integration

--- 01-nft-minter/frontend/src/App.tsx
+++ 01-nft-minter/frontend/src/App.tsx
@@ -4,16 +4,26 @@ import { queryClient } from "./config/tanstack-query";
 import { networkConfig } from "./config/network";
 import { ConnectButton } from "./components/ConnectButton";
 import { MintNftButton } from "./components/MintNftButton";
+import { NftList } from "./components/NftList";
 
 export function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <SuiClientProvider networks={networkConfig} defaultNetwork="localnet">
         <WalletProvider autoConnect>
-          <div className="p-4">
-            <div className="flex flex-wrap gap-2">
-              <ConnectButton />
-              <MintNftButton />
+          <div className="@container">
+            <div className="mx-auto @3xl:w-[48rem] @4xl:px-0 px-4 relative">
+              <div className="p-4 space-y-8">
+                <div className="flex flex-wrap gap-2">
+                  <ConnectButton />
+                  <MintNftButton />
+                </div>
+
+                <div className="space-y-2">
+                  <div className="text-2xl font-semibold">My NFTs</div>
+                  <NftList />
+                </div>
+              </div>
             </div>
           </div>
         </WalletProvider>
---
+++ 01-nft-minter/frontend/src/components/NftList.tsx
@@ -0,0 +1,40 @@
+export function NftList() {
+  const nfts: {
+    photoUrl: string;
+    name: string;
+  }[] = [
+    {
+      photoUrl:
+        "https://imgs.search.brave.com/w1uwMJMIrXocUzDrw3h9NHyaUnhcy52ppflGKfeFcZA/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMudW5zcGxhc2gu/Y29tL3Bob3RvLTE1/NzAxMzg3NDQ3NzEt/NTViYTFmM2M3NDNk/P2ZtPWpwZyZxPTYw/Jnc9MzAwMCZpeGxp/Yj1yYi00LjEuMCZp/eGlkPU0zd3hNakEz/ZkRCOE1IeHpaV0Z5/WTJoOE5IeDhjMjFo/Ykd3bE1qQjBiM2R1/ZkdWdWZEQjhmREI4/Zkh3dw",
+      name: "Small town. Bricks. Beautiful town",
+    },
+    {
+      photoUrl:
+        "https://imgs.search.brave.com/xibCr9QAKky8jpywPPRA2rzHx_lsX7fNvJJc8LRBgd0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90My5m/dGNkbi5uZXQvanBn/LzAxLzIxLzg2Lzgy/LzM2MF9GXzEyMTg2/ODI1OF9SOXlrRmRD/Mzhtbk91aTc0ZXlt/QWs4N1dSb2lDZ21I/eC5qcGc",
+      name: "Pretty town. Nft on Sui",
+    },
+  ];
+
+  if (!nfts.length) return <div>You haven't minted any NFTs yet 😔</div>;
+
+  return (
+    <div className="w-full flex-wrap gap-2 grid grid-cols-2 sm:grid-cols-4">
+      {nfts.map((nft, index) => (
+        <NftListItem key={index} src={nft} />
+      ))}
+    </div>
+  );
+}
+
+type NftListItemProps = {
+  src: { photoUrl: string; name: string };
+};
+
+function NftListItem({ src }: NftListItemProps) {
+  return (
+    <div className="rounded-md flex flex-col overflow-hidden border w-full">
+      <img src={src.photoUrl} className="w-full object-cover aspect-square" />
+      <div className="px-3 py-2 wrap-normal">{src.name}</div>
+    </div>
+  );
+}
