52bf5974bc906358538086d18bac5912ac6cbe6d
daniilkananenka <neokv210@gmail.com>
2025-09-30 21:48:05 +0300

    fix: move API key to env

--- 03-guestbook/frontend/src/config/enoki.ts
+++ 03-guestbook/frontend/src/config/enoki.ts
@@ -1,5 +1,5 @@
 import { EnokiClient } from "@mysten/enoki";
 
 export const enokiClient = new EnokiClient({
-  apiKey: "enoki_private_3eb1a98cbca7edaa0a3fb40b2f006d55",
+  apiKey: import.meta.env.VITE_ENOKI_PRIVATE_API_KEY,
 });
